5f310fae3e9d2aa7019781d077dd76db180cd022
James Basile <jbasile145@gmail.com>
2019-07-31 17:40:57 -0400

    movement is now working

--- cs_buildweek/src/components/MapWindow/MapWindow.js
+++ cs_buildweek/src/components/MapWindow/MapWindow.js
@@ -5,33 +5,93 @@ import axios from 'axios';
 
 export class MapWindow extends Component {
     componentDidMount() {
-        this.initData();
+        this.initRoomData();
+        this.initStatus();
     }
 
-    initData = async() => {
+    initRoomData = async () => {
         try {
             let res = await axios({
                 url: 'https://lambda-treasure-hunt.herokuapp.com/api/adv/init/',
                 method: 'get',
-                timeout: 5000,
+                timeout: 1000,
                 headers: {
                     Authorization: 'Token 61577c390423683a9bfc0e9a28456c70536f046b'
                 }
             });
-            console.log('Status:', res.status);
-            console.log('Data:', res.data);
-            console.log('Room ID:', res.data.room_id);
+            console.log('Room Data', [{ 'Status:': res.status }, { 'Data:': res.data }, { 'Room ID:': res.data.room_id }]);
 
             return res.data
-        } catch(err) {
+        } catch (err) {
             console.log(err)
         }
     };
 
+    initStatus = async () => {
+        try {
+            let res = await axios({
+                url: 'https://lambda-treasure-hunt.herokuapp.com/api/adv/status/',
+                method: 'post',
+                timeout: 2000,
+                headers: {
+                    Authorization: 'Token 61577c390423683a9bfc0e9a28456c70536f046b'
+                }
+            });
+            console.log('Player Status', [{ 'Status:': res.status }, { 'Data:': res.data }, { 'Player Name:': res.data.name }]);
+
+            return res.data
+        } catch (err) {
+            console.log(err)
+        }
+    };
+
+    movePlayer = async (direction, next_room_id = null) => {
+        let axiosCall;
+        if (next_room_id !== null) {
+            axiosCall = {
+                url: 'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/',
+                method: 'post',
+                headers: {
+                    Authorization: 'Token 61577c390423683a9bfc0e9a28456c70536f046b'
+                },
+                data: {
+                    direction: direction,
+                    next_room_id: next_room_id.toString()
+                }
+            }
+        } else {
+            axiosCall = {
+                url: 'https://lambda-treasure-hunt.herokuapp.com/api/adv/move/',
+                method: 'post',
+                headers: {
+                    Authorization: 'Token 61577c390423683a9bfc0e9a28456c70536f046b'
+                },
+                data: {
+                    direction: direction
+                }
+            }
+        }
+        try {
+            let res = await axios(axiosCall);
+            console.log('Move', [{ 'Status:': res.status }, { 'Data:': res.data }])
+            setTimeout(() => {
+                this.initStatus();
+            }, res.data.cooldown * 1000);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     render() {
-        return(
+        return (
             <>
-            <p>CS BUILD WEEK</p>
+                <h1>CS BUILD WEEK</h1>
+                <div>
+                    <button onClick={() => this.movePlayer('n')}>North</button>
+                    <button onClick={() => this.movePlayer('s')}>South</button>
+                    <button onClick={() => this.movePlayer('e')}>East</button>
+                    <button onClick={() => this.movePlayer('w')}>West</button>
+                </div>
             </>
         )
     }
